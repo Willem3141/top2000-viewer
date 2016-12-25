3cc58e7580dd8dfcfbda8d152a3edb15050c0ef4
Willem Sonke <willem@wimiso.nl>
2016-12-25 09:22:12 +0100

    Add 2016 presenters

--- top2016/presenters.js
+++ top2016/presenters.js
@@ -1,13 +1,13 @@
 [
-    {"name": "Corné Klijn", "hour": 0},
-    {"name": "Jasper de Vries", "hour": 2},
+    {"name": "Evelien de Bruijn", "hour": 0},
+    {"name": "Marisa Heutink", "hour": 2},
     {"name": "Frank van ’t Hof", "hour": 4},
     {"name": "Gerard Ekdom", "hour": 6},
     {"name": "Bart Arens", "hour": 9},
     {"name": "Gijs Staverman", "hour": 12},
     {"name": "Rob Stenders", "hour": 14},
-    {"name": "Wouter van der Goes", "hour": 16},
-    {"name": "Bert Haandrikman", "hour": 18},
+    {"name": "Ruud de Wild", "hour": 16},
+    {"name": "Wouter van der Goes", "hour": 18},
     {"name": "Stefan Stasse", "hour": 20},
     {"name": "Jan-Willem Roodbeen", "hour": 22}
 ]
