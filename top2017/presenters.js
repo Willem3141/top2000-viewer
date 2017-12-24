2f5304a9f051dce8b64170d31d84a473f1158c1a
Willem Sonke <willem@wimiso.nl>
2017-12-24 17:15:36 +0100

    Add Top 2000 for 2017

---
+++ top2017/presenters.js
@@ -0,0 +1,13 @@
+[
+    {"name": "Evelien de Bruijn", "hour": 0},
+    {"name": "Cielke Sijben", "hour": 2},
+    {"name": "Frank van 't Hof", "hour": 4},
+    {"name": "Gerard Ekdom", "hour": 6},
+    {"name": "Bart Arens", "hour": 9},
+    {"name": "Gijs Staverman", "hour": 12},
+    {"name": "Annemieke Schollaardt", "hour": 14},
+    {"name": "Ruud de Wild", "hour": 16},
+    {"name": "Wouter van der Goes", "hour": 18},
+    {"name": "Stefan Stasse", "hour": 20},
+    {"name": "Jan-Willem Roodbeen", "hour": 22}
+]
