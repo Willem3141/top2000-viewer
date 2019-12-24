c9bae1707fa6ff06eb5dbf3b2d051b0c45828ea2
Willem Sonke <willem@wimiso.nl>
2019-12-24 23:20:09 +0100

    Update presenters, Stefan Stasse is ill :(

--- top2019/presenters.js
+++ top2019/presenters.js
@@ -2,13 +2,13 @@
     {"name": "Leo Blokhuis", "hour": 0},
     {"name": "Rick van Velthuijzen", "hour": 2},
     {"name": "Frank van 't Hof", "hour": 4},
-    {"name": "Annemieke Schollaardt", "hour": 6},
+    {"name": "Emmely de Wild", "hour": 6},
     {"name": "Jan-Willem Roodbeen", "hour": 8},
     {"name": "Bart Arens", "hour": 10},
     {"name": "Gijs Staverman", "hour": 12},
     {"name": "Rob Stenders", "hour": 14},
     {"name": "Ruud de Wild", "hour": 16},
     {"name": "Wouter van der Goes", "hour": 18},
-    {"name": "Stefan Stasse", "hour": 20},
+    {"name": "Annemieke Schollaardt", "hour": 20},
     {"name": "Paul Rabbering", "hour": 22}
 ]
